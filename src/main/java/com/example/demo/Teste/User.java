566c9625f1a3fc8dc336600a71925f9381fc9028
Tarciso dos Santos de Oliveira <tarcisoliveira28@gmail.com>
2026-06-03 18:59:33 -0300

    implementando e procurando o erro da tabela

--- src/main/java/com/example/demo/Teste/User.java
+++ src/main/java/com/example/demo/Teste/User.java
@@ -1,17 +1,32 @@
 package com.example.demo.Teste;
 
 import java.util.Objects;
-import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+   
+  @Entity 
+  @Table(name="tb_user")
 public class User implements Serializable {
 	
 	
+	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+	
 	public User() {
 		super();
 	}
--- src/main/java/com/example/demo/Teste/resource/Userresource.java
+++ src/main/java/com/example/demo/Teste/resource/Userresource.java
@@ -1,9 +1,11 @@
 package com.example.demo.Teste.resource;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+
 
 
 import com.example.demo.Teste.User;
@@ -11,7 +13,7 @@ import com.example.demo.Teste.User;
 @RestController
 @RequestMapping(value="/users")
 
-public class Userresource {
+public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		
